54403277bbc367363e428322a66412a9892b00fc
reepias <adthasit.em@gmail.com>
2019-06-28 15:56:25 +0700

    chore/prettier config

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+// prettier.config.js or .prettierrc.js
+module.exports = {
+  printWidth: 80,
+  trailingComma: 'es5',
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  jsxBracketSameLine: false,
+}
